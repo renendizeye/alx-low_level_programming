000fd3acadcb179aaebd687a76186485077e891c
renendizeye <ndizeyeirene777@gmail.com>
2022-03-24 14:26:56 -0700

    Pointers_arrays_strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,7 +8,8 @@
  * @n: nuumber of bytes to receive
  * Return: char
  */
-char *_strncat(char *dest, char*src, int n)
+
+char *_strncat(char *dest, char *src, int n)
 {
 	unsigned int i = 0;
 	int j = 0;
